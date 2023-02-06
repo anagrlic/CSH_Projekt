5fdda2a2bd946716292194149aabf1f37ae0fad1
ana.grlic <ana.grlic@UNIVULAP348>
2023-02-06 16:12:43 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace CSH_Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDownHours = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnstart.Location = new System.Drawing.Point(29, 262);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(120, 45);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStop.Location = new System.Drawing.Point(210, 262);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(121, 42);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Zaustavi";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblHours.Location = new System.Drawing.Point(79, 20);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(90, 75);
+            this.lblHours.TabIndex = 4;
+            this.lblHours.Text = "00";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSeconds.Location = new System.Drawing.Point(345, 20);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(90, 75);
+            this.lblSeconds.TabIndex = 5;
+            this.lblSeconds.Text = "00";
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblMinutes.Location = new System.Drawing.Point(210, 20);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(90, 75);
+            this.lblMinutes.TabIndex = 6;
+            this.lblMinutes.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(166, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 75);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(293, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 75);
+            this.label6.TabIndex = 8;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(35, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 31);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Sekunda:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(35, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 31);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Minuta:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(35, 122);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 31);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Sat:";
+            // 
+            // numericUpDownHours
+            // 
+            this.numericUpDownHours.Location = new System.Drawing.Point(170, 133);
+            this.numericUpDownHours.Name = "numericUpDownHours";
+            this.numericUpDownHours.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDownHours.TabIndex = 16;
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(170, 214);
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDownSeconds.TabIndex = 17;
+            // 
+            // numericUpDownMinutes
+            // 
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(170, 170);
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDownMinutes.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(507, 336);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.numericUpDownSeconds);
+            this.Controls.Add(this.numericUpDownHours);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnstart);
+            this.Name = "Form1";
+            this.Text = "   ";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDownHours;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSH_Projekt
+{
+    public partial class Form1 : Form
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+        private int trig = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            hours = Convert.ToInt32(Math.Round(numericUpDownHours.Value, 3));
+            minutes = Convert.ToInt32(Math.Round(numericUpDownMinutes.Value, 3));
+            seconds = Convert.ToInt32(Math.Round(numericUpDownSeconds.Value, 3));
+
+            lblHours.Text = hours.ToString();
+            lblMinutes.Text = minutes.ToString();
+            lblSeconds.Text = seconds.ToString();
+
+            if (trig == 0)
+            {
+                timer1 = new Timer();
+                timer1.Tick += Timer1_Tick;
+                timer1.Interval = 1000; //1 sekunda
+                trig = 1;
+            }
+
+            timer1.Start();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            seconds--;
+            if (seconds <= 0)
+            {
+                if (minutes > 0 || hours > 0)
+                {
+                    if (minutes == 0)
+                    {
+                        if (hours >= 0)
+                        {
+                            hours = hours - 1;
+                            minutes = 60;
+                            if (hours < 0)
+                            {
+                                hours = 0;
+                            }
+                        }
+                    }
+
+                    minutes = minutes - 1;
+                    seconds = 59;
+                    timer1 = new Timer();
+                    timer1.Interval = 1000;
+                    timer1.Start();
+                    lblHours.Text = hours.ToString();
+                    lblMinutes.Text = minutes.ToString();
+                    lblSeconds.Text = seconds .ToString();
+
+                    if (minutes < 0)
+                    {
+                        minutes = 0;
+                    }
+                }
+                if (seconds < 0)
+                {
+                    seconds = 0;
+                }
+                timer1.Stop();
+            }
+
+            if (seconds < 60 && minutes < 5)
+            {
+                if (seconds % 2 == 0)
+                {
+                    lblHours.ForeColor = Color.Red;
+                    lblSeconds.ForeColor = Color.Red;
+                    lblMinutes.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblHours.ForeColor = Color.Black;
+                    lblMinutes.ForeColor = Color.Black;
+                    lblSeconds.ForeColor = Color.Black;
+                }
+                
+            }
+
+            lblHours.Text = hours.ToString();
+            lblMinutes.Text = minutes.ToString();
+            lblSeconds.Text = seconds.ToString();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+    }
+}
